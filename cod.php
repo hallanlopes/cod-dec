169264ec80b557cbae55e844d12c6434ea87f47f
hallanlopes <hjlfagundes@restinga.ifrs.edu.br>
2017-07-06 09:10:29 -0300

    Update cod.php

--- cod.php
+++ cod.php
@@ -1,29 +1,29 @@
 <?php
 
-$string=$_GET['msg']; // Variavel da mensagem que será criptografada
-$newstring= " "; // Variavel que recebe o resultado da codificação
-$chave=$_GET['sp']; // Variavel que recebe o numero de casas que ira se deslocar 
+$string=$_GET['msg']; // Variável da mensagem que será criptografada
+$newstring= " "; // Variável que recebe o resultado da codificação
+$chave=$_GET['sp']; // Variável que recebe o numero de casas que ira se deslocar 
 
 for($i=0;$i<strlen($string);$i++){ //Faz o teste da codificação
 	$ascii=ord($string[$i]); //Retorna o valor ASCII do caractere
 	$ascii=$ascii + $chave; //Soma o valor ASCII com a chave
-	$newstring[$i]=chr($ascii); //Torna o caractere da variavel ASCCI em um valor especifico 
+	$newstring[$i]=chr($ascii); //Torna o caractere da variável ASCCI em um valor específico 
 	
-	echo "<p>" . $ascii . "</p>"; //Imprime o valor da variavel, com intuito de achar erros
-    echo "<p>" . $string[$i] . "</p>"; //Imprime o valor da variavel, com intuito de achar erros
+	echo "<p>" . $ascii . "</p>"; //Imprime o valor da variável, com intuito de achar erros
+    echo "<p>" . $string[$i] . "</p>"; //Imprime o valor da variável, com intuito de achar erros
 
 
 }
-       echo "<p>" . $newstring . "</p>";//Imprime o valor da variavel codificada
+       echo "<p>" . $newstring . "</p>";//Imprime o valor da variável codificada
 ?>
 
 <html>
 <body>
 	<form>
-		<textarea name=msg></textarea>
+		<textarea name=msg></textarea> <!-- Caixa de mensagem á ser codificada -->
 			<td>
-				<input type=text size=2 name=sp>
-				<input type=submit name=submit value=codificar>
+				<input type=text size=2 name=sp> <!-- Caixa de escolha de chave -->
+				<input type=submit name=submit value=codificar> <!-- Botão de codifica -->
 					</form> 
 </body>
 </html>
